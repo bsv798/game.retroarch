cc76245bbfb4ee57e83d8227510c1a94237b02e3
Sergey Bizin <bsv798@gmail.com>
2023-12-16 19:18:38 +0700

    Add KODI build arg to docker build

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ build(){
   MESA=$(wget -O - https://github.com/LibreELEC/LibreELEC.tv/raw/${LIBREELEC}/packages/graphics/mesa/package.mk | grep PKG_VERSION= | sed -E 's/.*="(.*)"/mesa-\1/')
   KODI=$(wget -O - https://github.com/LibreELEC/LibreELEC.tv/raw/${LIBREELEC}/packages/mediacenter/kodi/package.mk | grep PKG_VERSION= | sed -E 's/.*="(.*)"/\1/')
 
-  docker build  . --build-arg VULKAN="${VULKAN}" --build-arg MESA=${MESA} --build-arg RETROARCH="${RETROARCH}" --tag game.retroarch
+  docker build  . --build-arg VULKAN="${VULKAN}" --build-arg MESA=${MESA} --build-arg KODI="${KODI}" --build-arg RETROARCH="${RETROARCH}" --tag game.retroarch
   docker run --rm -d --name game.retroarch game.retroarch tail -f /dev/null
   docker cp game.retroarch:/buildkit/game.retroarch.zip .
   docker cp game.retroarch:/buildkit/game.libretro.mupen64plus-nx.zip .
